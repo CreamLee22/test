6104824e36f08c16deb407e09c10ded44e261466
CreamLee22 <1562022208@qq.com>
2021-02-24 00:03:56 +0800

    test1

---
+++ src/main/java/test.java
@@ -0,0 +1,11 @@
+/**
+ * @Author: LiKai
+ * @Date: 2021/2/23 23:52
+ * @Description:
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("1111");
+
+    }
+}
